ccedb4e22674dd89dd8b663c75293b63deb4ebb0
Thales Ribeiro <thales.gcr05@gmail.com>
2025-04-30 09:10:29 -0300

    Delete cosanpa_forms/cosanpa_forms directory

--- cosanpa_forms/cosanpa_forms/src/utils/formatarCPF.js
+++
@@ -1,7 +0,0 @@
-export default function formatarCPF(cpf) {
-  let nums = cpf.replace(/\D/g, '').slice(0, 11);
-  nums = nums.replace(/(\d{3})(\d)/, '$1.$2');
-  nums = nums.replace(/(\d{3})(\d)/, '$1.$2');
-  nums = nums.replace(/(\d{3})(\d{1,2})$/, '$1-$2');
-  return nums;
-}
